472f704f569f5a39fbdda688e5d1a7dda7a6b6d9
suki-sahota <46114711+suki-sahota@users.noreply.github.com>
2019-02-19 21:26:43 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    firstName: 'First',
+    lastName: 'Last',
+    email: '',
+    ticketQuantity: 1,
+    ticketType: 'general',
+    referrals: [],
+    specialRequests: '',
+    purchaseAgreementSigned: false
+  },
+  computed: {
+    fullName: function() {
+      if (this.firstName && this.lastName) {
+        return this.firstName + ' ' + this.lastName;
+      } else {
+        return this.firstName || this.lastName;
+      }
+    }
+  },
+  computed: {
+    ticketDescription: function() {
+      let readableTicketType = 'General Admission'; 
+      if (this.ticketType === 'vip') {
+        readableTicketType = 'VIP';
+      }
+      
+      let ticketPluralization = 'tickets';
+      if (this.ticketQuantity === 1) {
+        ticketPluralization = 'ticket';
+      }
+      
+      return this.ticketQuantity + ' ' + readableTicketType + ' ' + ticketPluralization;
+    }
+  }
+});
